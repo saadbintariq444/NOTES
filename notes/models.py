ba382364a15305087d1454a2caa5c050c944e1f3
saad <saadbintariq444@gmail.com>
2020-04-12 12:33:21 +0500

    first model created

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+#models are actually database
+class Note(models.Model):
+   title=models.CharField(max_length=150)
+   content=models.TextField()
+   created_at=models.DateTimeField(auto_now=True)
+   updated_at=models.DateTimeField(auto_now=True)
+   
+   def __str__(self):
+      return self.title
+    
